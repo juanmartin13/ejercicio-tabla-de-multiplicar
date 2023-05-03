afd64c26accffaf29ff3f665bc61a480f97c7511
juanmartin13 <132444315+juanmartin13@users.noreply.github.com>
2023-05-03 11:14:19 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+let variable2 = 0
+let variable1 = 0
+input.onButtonPressed(Button.A, function () {
+    variable2 = 2
+    basic.showNumber(variable2)
+    variable1 = randint(0, 10)
+    basic.showNumber(variable1)
+    basic.showNumber(variable1 * variable2)
+})
